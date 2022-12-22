ff6dae77dd7d964fd4ae5e5ad1614ed3e3441182
Ayomide Ayodele-Soyebo <midesuperbest@gmail.com>
2022-12-22 18:53:53 +0100

    A minor correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length > 2) {
+if (process.argv.length > 3) {
+  console.log('Arguments found');
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('No argument');
