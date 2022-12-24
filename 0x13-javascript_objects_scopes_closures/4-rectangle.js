1c4bf56e1f5b442ccd2d21441842c65309c5be28
Ayomide Ayodele-Soyebo <midesuperbest@gmail.com>
2022-12-24 05:26:45 +0100

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const pattern = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(pattern);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
