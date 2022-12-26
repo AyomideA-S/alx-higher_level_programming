b16c0f474e85012603b5c332cadd3e35c36fc483
Ayomide Ayodele-Soyebo <midesuperbest@gmail.com>
2022-12-26 03:34:34 +0100

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  Lists all states from the database hbtn_0e_0_usa """
+"""  Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -7,7 +7,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("""
+    SELECT * FROM states
+    ORDER BY states.id ASC
+    """)
     rows = cur.fetchall()
     for row in rows:
         print(row)
