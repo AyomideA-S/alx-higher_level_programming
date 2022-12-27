114429a38a76fd171da364466a3b1d697beee645
Ayomide Ayodele-Soyebo <midesuperbest@gmail.com>
2022-12-27 09:54:47 +0100

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,3 +11,57 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        ''' Returning private attribute '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        ''' Setting private attribute '''
+        self.setter_validation("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        ''' Returning private attribute '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        ''' Setting private attribute '''
+        self.setter_validation("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        ''' Returning private attribute '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        ''' Setting private attribute '''
+        self.setter_validation("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        ''' Returning private attribute '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        ''' Setting private attribute '''
+        self.setter_validation("y", value)
+        self.__y = value
+
+    @staticmethod
+    def setter_validation(attribute, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "x" or attribute == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(attribute))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(attribute))
